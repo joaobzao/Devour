6d61826fc4b07819b69f9b0d5449da15f78612c6
joaobzao <jmmbzao@gmail.com>
2020-09-27 02:20:05 +0100

    Create project devour

---
+++ app/src/main/java/com/bronzes/devour/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bronzes.devour
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
