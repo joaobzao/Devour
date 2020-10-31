61e550ff83a52c400c4d91a543abd25c358d00ac
joaobzao <jmmbzao@gmail.com>
2020-10-31 19:26:00 +0000

    Add form screen to add meal

---
+++ app/src/main/java/com/bronzes/devour/meal/AddForm.kt
@@ -0,0 +1,128 @@
+package com.bronzes.devour.meal
+
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.savedinstancestate.savedInstanceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AddForm() {
+    Box(Modifier.fillMaxSize()) {
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            val name = savedInstanceState(saver = TextFieldValue.Saver) {
+                TextFieldValue("")
+            }
+            val description = savedInstanceState(saver = TextFieldValue.Saver) {
+                TextFieldValue("")
+            }
+            val location = savedInstanceState(saver = TextFieldValue.Saver) {
+                TextFieldValue("")
+            }
+            val type = savedInstanceState(saver = TextFieldValue.Saver) {
+                TextFieldValue("")
+            }
+            val rate = savedInstanceState(saver = TextFieldValue.Saver) {
+                TextFieldValue("")
+            }
+
+            Spacer(modifier = Modifier.preferredHeight(44.dp))
+
+            OutlinedTextField(
+                value = name.value,
+                onValueChange = { name.value = it },
+                label = {
+                    ProvideEmphasis(emphasis = AmbientEmphasisLevels.current.medium) {
+                        Text(
+                            text = "Name",
+                            style = MaterialTheme.typography.body1
+                        )
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+
+            OutlinedTextField(
+                value = description.value,
+                onValueChange = { description.value = it },
+                label = {
+                    ProvideEmphasis(emphasis = AmbientEmphasisLevels.current.medium) {
+                        Text(
+                            text = "Description",
+                            style = MaterialTheme.typography.body1
+                        )
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+
+            OutlinedTextField(
+                value = location.value,
+                onValueChange = { location.value = it },
+                label = {
+                    ProvideEmphasis(emphasis = AmbientEmphasisLevels.current.medium) {
+                        Text(
+                            text = "Location",
+                            style = MaterialTheme.typography.body1
+                        )
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+
+            OutlinedTextField(
+                value = type.value,
+                onValueChange = { type.value = it },
+                label = {
+                    ProvideEmphasis(emphasis = AmbientEmphasisLevels.current.medium) {
+                        Text(
+                            text = "Type",
+                            style = MaterialTheme.typography.body1
+                        )
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+
+            OutlinedTextField(
+                value = rate.value,
+                onValueChange = { rate.value = it },
+                label = {
+                    ProvideEmphasis(emphasis = AmbientEmphasisLevels.current.medium) {
+                        Text(
+                            text = "Rate",
+                            style = MaterialTheme.typography.body1
+                        )
+                    }
+                }
+            )
+        }
+
+        Spacer(modifier = Modifier.preferredHeight(44.dp))
+        
+        Column(Modifier.fillMaxWidth().align(Alignment.BottomCenter)) {
+            Button(
+                enabled = true,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp),
+                onClick = { println("Add Meal") }
+            ) {
+                Text("Add Meal!")
+            }
+        }
+    }
+}
--- app/src/main/java/com/bronzes/devour/meal/MealActivity.kt
+++ app/src/main/java/com/bronzes/devour/meal/MealActivity.kt
@@ -2,11 +2,24 @@ package com.bronzes.devour.meal
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Border
+import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.savedinstancestate.savedInstanceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.bronzes.devour.ui.DevourTheme
 
@@ -17,7 +30,7 @@ class MealActivity : AppCompatActivity() {
             DevourTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    AddFormContainer()
                 }
             }
         }
@@ -25,14 +38,19 @@ class MealActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AddFormContainer() {
+   Column(
+       modifier = Modifier.fillMaxWidth().background(MaterialTheme.colors.background),
+       horizontalAlignment = Alignment.CenterHorizontally
+   ) {
+       AddForm()
+   }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun DefaultPreview() {
     DevourTheme {
-        Greeting("Android")
+        AddFormContainer()
     }
 }
